f86e15de2d10d58fb4f2784fa54d77709493c7c9
SHDGMSQ <sh.dg.msq@mail.ru>
2022-12-06 22:16:53 +0300

    Commit with init Todolists and tasks

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,18 +1,36 @@
 <template>
-  <div id="app">
+  <div class="app">
+    <Todolist title="What to learn" v-bind:tasks="tasks1"/>
+    <Todolist title="What to buy" v-bind:tasks="tasks2"/>
   </div>
 </template>
 
 <script>
 
+import Todolist from "@/components/Todolist";
 export default {
   name: 'App',
-  components: {
+  components: {Todolist},
+  data(){
+    return {
+      tasks1: [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'VUE', isDone: false},
+        {id: 3, title: 'NODE', isDone: false},
+      ],
+      tasks2: [
+        {id: 1, title: 'HELLO', isDone: true},
+        {id: 1, title: 'WORLD', isDone: false},
+        {id: 1, title: 'YO', isDone: true},
+      ],
+    }
   }
 }
 </script>
 
 <style>
-#app {
+.app {
+  padding: 30px;
+  display: flex;
 }
 </style>
---
+++ client/src/components/Todolist.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="todolist">
+    <h3>{{title}}<button>x</button></h3>
+    <div>
+      <input>
+      <button>+</button>
+    </div>
+    <ul>
+      <li><input type="checkbox" :checked="tasks[0].isDone"><span>{{tasks[0].title}}</span><button>x</button></li>
+      <li><input type="checkbox" :checked="tasks[1].isDone"><span>{{tasks[1].title}}</span><button>x</button></li>
+      <li><input type="checkbox" :checked="tasks[2].isDone"><span>{{tasks[2].title}}</span><button>x</button></li>
+    </ul>
+    <button>All</button>
+    <button>Active</button>
+    <button>Completed</button>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+
+  name: "Todolist",
+  props: {
+    title: String,
+    tasks: {
+      id: Number,
+      title: String,
+      isDone: Boolean
+    },
+  }
+}
+</script>
+
+<style>
+.todolist {
+  padding: 30px;
+}
+</style>
